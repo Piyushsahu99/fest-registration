b0ac4caf800aac5fc2a72bc958ec8aa98ac99179
Piyush sahu <156942206+Piyushsahu99@users.noreply.github.com>
2024-09-27 09:13:00 +0530

    Update event_database.sql
    Added a registration page

--- event_database.sql
+++ event_database.sql
@@ -13,6 +13,21 @@ CREATE TABLE registrations (
     event VARCHAR(20) NOT NULL
 );
 
+DELIMITER //
+CREATE PROCEDURE add_registration(
+    IN p_roll VARCHAR(20),
+    IN p_fullname VARCHAR(50),
+    IN p_email VARCHAR(50),
+    IN p_phno VARCHAR(15),
+    IN p_stream VARCHAR(10),
+    IN p_event VARCHAR(20)
+)
+BEGIN
+    INSERT INTO registrations (roll, fullname, email, phno, stream, event)
+    VALUES (p_roll, p_fullname, p_email, p_phno, p_stream, p_event);
+END //
+DELIMITER ;
+
 show tables;
 
 DESCRIBE registrations;
